74a8be6a98c0073116b5544ebfeda4186708d2a9
Hazen Eckert <hazen.eckert@gmail.com>
2025-03-25 21:04:38 -0500

    Added formatting

--- include/bloom_filter.hpp
+++ include/bloom_filter.hpp
@@ -2,16 +2,14 @@
 #define PDS_BLOOM_FILTER_HPP
 
 #include "hash.hpp"
-#include <vector>
-#include <ranges>
 #include <bit>
+#include <ranges>
+#include <vector>
 // use fastrange for faster modulo or libdivide
-// use chache local HashGenerator
-// use SIMD HashGenerator
-// libpopcnt for counting bits
-// highway for simd
-// page tasble awareness?
-// Bloom Filter implementations
+// Options for prime number and power-of-2 sized bitvectors. Apparently you want
+// to mod a hash by a prime. use chache local HashGenerator use SIMD
+// HashGenerator libpopcnt for counting bits highway for simd page tasble
+// awareness? Bloom Filter implementations
 // https://github.com/peterboncz/bloomfilter-bsd/tree/master
 // https://github.com/apache/impala/blob/master/be/src/util/bloom-filter.h
 // https://github.com/facebook/rocksdb/blob/88bc91f3cc2b492b8a45ba2c49650f527df97ad8/util/dynamic_bloom.h
@@ -24,135 +22,163 @@
 
 namespace pds {
 
-template <typename Key, hash::HashGenerator<Key> HashGen = pds::hash::default_hash_generator<Key>, std::unsigned_integral WordType = uint8_t, typename Allocator = std::allocator<WordType>>
+template <typename Key,
+          hash::HashGenerator<Key> HashGen =
+              pds::hash::default_hash_generator<Key>,
+          std::unsigned_integral WordType = uint8_t,
+          typename Allocator = std::allocator<WordType>>
 class bloom_filter {
 public:
-    using key_type = Key;
-    using hash_type = HashGen::hash_type;
-    using size_type = size_t;
-    using word_type = WordType;
-    using allocator_type = Allocator;
-    using hash_generator_type = HashGen;
-    
-    static constexpr size_type bits_per_word = std::numeric_limits<word_type>::digits;
-    static constexpr size_type bits_per_word_log2 = std::countr_zero(bits_per_word);
-    static constexpr word_type word_mask = std::numeric_limits<word_type>::max();
-    
-    static constexpr size_t optimal_num_bits(size_t input_size, double false_positive_probability) {
-        return static_cast<size_t>(-std::log(false_positive_probability)*input_size/std::pow(std::log(2.0),2));
+  using key_type = Key;
+  using hash_type = HashGen::hash_type;
+  using size_type = size_t;
+  using word_type = WordType;
+  using allocator_type = Allocator;
+  using hash_generator_type = HashGen;
+
+  static constexpr size_type bits_per_word =
+      std::numeric_limits<word_type>::digits;
+  static constexpr size_type bits_per_word_log2 =
+      std::countr_zero(bits_per_word);
+  static constexpr word_type word_mask = std::numeric_limits<word_type>::max();
+
+  static constexpr size_t optimal_num_bits(size_t input_size,
+                                           double false_positive_probability) {
+    return static_cast<size_t>(-std::log(false_positive_probability) *
+                               input_size / std::pow(std::log(2.0), 2));
+  }
+  static constexpr size_t
+  optimal_num_hashes(size_t input_size, double false_positive_probability) {
+    return static_cast<size_t>(
+        optimal_num_bits(input_size, false_positive_probability) *
+        std::log(2.0) / input_size);
+  }
+  static constexpr size_t approximate_cardinality(size_t bit_capacity,
+                                                  size_t num_set_bits,
+                                                  size_t hashes_per_key) {
+    return static_cast<size_t>(
+        -(static_cast<double>(bit_capacity) /
+          static_cast<double>(hashes_per_key)) *
+        std::log(1 - static_cast<double>(num_set_bits) /
+                         static_cast<double>(bit_capacity)));
+  }
+  static constexpr double false_positive_probability(size_t bit_capacity,
+                                                     size_t input_size,
+                                                     size_t hashes_per_key) {
+    return std::pow(1.0 - std::exp(-hashes_per_key * input_size / bit_capacity),
+                    hashes_per_key);
+  }
+
+  bloom_filter(size_t num_bits, size_t num_hashes,
+               const Allocator &alloc = Allocator())
+      : bit_array_((num_bits + word_mask) / bits_per_word, 0, alloc),
+        hash_generator_(num_hashes) {}
+  bloom_filter(size_t input_size, double false_positive_probability = 0.03,
+               const Allocator &alloc = Allocator())
+      : bit_array_(optimal_num_bits(input_size, false_positive_probability) >>
+                       bits_per_word_log2,
+                   0, alloc),
+        hash_generator_(
+            optimal_num_hashes(input_size, false_positive_probability)) {}
+  bloom_filter(size_t num_bits, HashGen hash_generator,
+               const Allocator &alloc = Allocator())
+      : bit_array_((num_bits + word_mask) / bits_per_word, 0, alloc),
+        hash_generator_(hash_generator) {}
+
+  template <typename InputIt>
+  void insert(InputIt first, InputIt last) noexcept {
+    for (auto it = first; it != last; ++it) {
+      insert(*it);
     }
-    static constexpr size_t optimal_num_hashes(size_t input_size, double false_positive_probability) {
-        return static_cast<size_t>(optimal_num_bits(input_size,false_positive_probability)*std::log(2.0)/input_size);
+  }
+  void insert(const Key &key) noexcept {
+    for (auto hash : hash_generator_.hashes(key)) {
+      auto bit_index = hash % (bit_array_.size() * bits_per_word);
+      bit_array_[bit_index >> bits_per_word_log2] |= 1
+                                                     << (bit_index & word_mask);
     }
-    static constexpr size_t approximate_cardinality(size_t bit_capacity, size_t num_set_bits, size_t hashes_per_key) {
-        return static_cast<size_t>(-(static_cast<double>(bit_capacity)/static_cast<double>(hashes_per_key))*std::log(1-static_cast<double>(num_set_bits)/static_cast<double>(bit_capacity)));
+  }
+  bool contains(const Key &key) const noexcept {
+    for (auto hash : hash_generator_.hashes(key)) {
+      auto bit_index = hash % (bit_array_.size() * bits_per_word);
+      if (!(bit_array_[bit_index >> bits_per_word_log2] &
+            (1 << (bit_index & word_mask))))
+        return false;
     }
-    static constexpr double false_positive_probability(size_t bit_capacity, size_t input_size, size_t hashes_per_key) {
-        return std::pow(1.0 - std::exp(-hashes_per_key*input_size / bit_capacity), hashes_per_key);
+    return true;
+  }
+  void clear() noexcept { std::fill(bit_array_.begin(), bit_array_.end(), 0); }
+
+  // Returns the number of bits in the underlying bit array.
+  size_t bit_capacity() const noexcept { return bit_array_.size(); }
+
+  // Returns the number of set bits in the underlying bit array.
+  size_t num_set_bits() const noexcept {
+    auto rv = bit_array_ | std::views::transform(std::popcount<word_type>);
+    return std::accumulate(rv.begin(), rv.end(), 0);
+  }
+
+  bool empty() const noexcept {
+    return std::all_of(bit_array_.begin(), bit_array_.end(),
+                       [](word_type x) { return x == 0; });
+  }
+
+  void
+  swap(const bloom_filter<Key, HashGen, WordType, Allocator> &other) noexcept {
+    bit_array_.swap(other.bit_array_);
+  }
+
+  size_t hashes_per_key() const noexcept {
+    return hash_generator_.hashes_per_key();
+  }
+
+  size_t approximate_cardinality() const noexcept {
+    return approximate_cardinality(bit_capacity(), num_set_bits(),
+                                   hashes_per_key());
+  }
+
+  double approximate_fpp() const noexcept {
+    return false_positive_probability(bit_capacity(), approximate_cardinality(),
+                                      hashes_per_key());
+  }
+
+  const std::vector<word_type> &data() { return bit_array_; }
+
+  bloom_filter<Key, HashGen, WordType, Allocator> &
+  operator&=(const bloom_filter<Key, HashGen, WordType, Allocator> &other) {
+    assert(other.bit_capacity() == bit_capacity());
+    for (size_t i = 0; i < bit_array_.size(); ++i) {
+      bit_array_[i] &= other.bit_array_[i];
     }
-    
-    bloom_filter(size_t num_bits, size_t num_hashes, const Allocator& alloc = Allocator())
-    : _bit_array((num_bits+word_mask)/bits_per_word,0,alloc),
-    _hash_generator(num_hashes)
-    {}
-    bloom_filter(size_t input_size, double false_positive_probability = 0.03, const Allocator& alloc = Allocator())
-    : _bit_array(optimal_num_bits(input_size,false_positive_probability)>>bits_per_word_log2,0,alloc),
-    _hash_generator(optimal_num_hashes(input_size,false_positive_probability))
-    {}
-    bloom_filter(size_t num_bits, HashGen hash_generator, const Allocator& alloc = Allocator())
-    : _bit_array((num_bits+word_mask)/bits_per_word,0,alloc),
-    _hash_generator(hash_generator)
-    {}
-    
-    template<typename InputIt>
-    void insert(InputIt first, InputIt last) noexcept {
-        for(auto it = first; it!=last; ++it) {
-            insert(*it);
-        }
+    return *this;
+  }
+
+  bloom_filter<Key, HashGen, WordType, Allocator>
+  operator&(const bloom_filter<Key, HashGen, WordType, Allocator> &other) {
+    bloom_filter<Key, HashGen, WordType, Allocator> res(*this);
+    res &= other;
+    return res;
+  }
+
+  bloom_filter<Key, HashGen, WordType, Allocator> &
+  operator|=(const bloom_filter<Key, HashGen, WordType, Allocator> &other) {
+    assert(other.bit_capacity() == bit_capacity());
+    for (size_t i = 0; i < bit_array_.size(); ++i) {
+      bit_array_[i] |= other.bit_array_[i];
     }
-    void insert(const Key& key) noexcept {
-        for (auto hash : _hash_generator.hashes(key)) {
-            auto bit_index = hash % (_bit_array.size()*bits_per_word);
-            _bit_array[bit_index>>bits_per_word_log2] |= 1 << (bit_index&word_mask);
-        }
-    }
-    bool contains(const Key& key) const noexcept {
-        for (auto hash : _hash_generator.hashes(key)) {
-            auto bit_index = hash % (_bit_array.size()*bits_per_word);
-            if(!(_bit_array[bit_index>>bits_per_word_log2] & (1<<(bit_index&word_mask)))) return false;
-        }
-        return true;
-    }
-    void clear() noexcept {
-        std::fill(_bit_array.begin(),_bit_array.end(), 0);
-    }
-    
-    // Returns the number of bits in the underlying bit array.
-    size_t bit_capacity() const noexcept {
-        return _bit_array.size();
-    }
-    
-    // Returns the number of set bits in the underlying bit array.
-    size_t num_set_bits() const noexcept {
-        auto rv = _bit_array | std::views::transform(std::popcount<word_type>);
-        return std::accumulate(rv.begin(),rv.end(),0);
-    }
-    
-    bool empty() const noexcept {
-        return std::all_of(_bit_array.begin(),_bit_array.end(), [] (word_type x){return x == 0;});
-    }
-    
-    void swap(const bloom_filter<Key,HashGen,WordType,Allocator>& other) noexcept {
-        _bit_array.swap(other._bit_array);
-    }
-    
-    size_t hashes_per_key() const noexcept {
-        return _hash_generator.hashes_per_key();
-    }
-    
-    size_t approximate_cardinality() const noexcept {
-        return approximate_cardinality(bit_capacity(), num_set_bits(), hashes_per_key());
-    }
-    
-    double approximate_fpp() const noexcept {
-        return false_positive_probability(bit_capacity(), approximate_cardinality(), hashes_per_key());
-    }
-    
-    const std::vector<word_type>& data() {
-        return _bit_array;
-    }
-    
-    bloom_filter<Key,HashGen,WordType,Allocator>& operator&=(const bloom_filter<Key,HashGen,WordType,Allocator>& other) {
-        assert(other.bit_capacity() == bit_capacity());
-        for (size_t i = 0; i<_bit_array.size(); ++i) {
-            _bit_array[i] &= other._bit_array[i];
-        }
-        return *this;
-    }
-    
-    bloom_filter<Key,HashGen,WordType,Allocator> operator&(const bloom_filter<Key,HashGen,WordType,Allocator>& other) {
-        bloom_filter<Key,HashGen,WordType,Allocator> res(*this);
-        res &= other;
-        return res;
-    }
-    
-    bloom_filter<Key,HashGen,WordType,Allocator>& operator|=(const bloom_filter<Key,HashGen,WordType,Allocator>& other) {
-        assert(other.bit_capacity() == bit_capacity());
-        for (size_t i = 0; i<_bit_array.size(); ++i) {
-            _bit_array[i] |= other._bit_array[i];
-        }
-        return *this;
-    }
-    
-    bloom_filter<Key,HashGen,WordType,Allocator> operator|(const bloom_filter<Key,HashGen,WordType,Allocator>& other) {
-        bloom_filter<Key,HashGen,Allocator> res(*this);
-        res |= other;
-        return res;
-    }
-    
+    return *this;
+  }
+
+  bloom_filter<Key, HashGen, WordType, Allocator>
+  operator|(const bloom_filter<Key, HashGen, WordType, Allocator> &other) {
+    bloom_filter<Key, HashGen, Allocator> res(*this);
+    res |= other;
+    return res;
+  }
+
 private:
-    std::vector<word_type,allocator_type> _bit_array;
-    hash_generator_type _hash_generator;
+  std::vector<word_type, allocator_type> bit_array_;
+  hash_generator_type hash_generator_;
 };
-}
+} // namespace pds
 #endif
--- include/hash.hpp
+++ include/hash.hpp
@@ -1,115 +1,116 @@
 #ifndef PDS_HASH_HPP
 #define PDS_HASH_HPP
 
+#include "MurmurHash3.h"
 #include <iostream>
 #include <limits>
 #include <queue>
 #include <random>
-#include <unordered_set>
 #include <ranges>
-#include "MurmurHash3.h"
+#include <unordered_set>
 
 namespace pds {
 namespace hash {
-template<typename T, typename Key>
-concept HashGenerator = requires (T t, size_t n, Key k) {
-    std::unsigned_integral<typename T::hash_type>;
-    T{n};
-    { t.hashes_per_key() } -> std::convertible_to<std::size_t>;
-    {t.hashes(k)} -> std::ranges::range;
-    requires std::same_as<std::ranges::range_value_t<decltype(t.hashes(k))>, typename T::hash_type>;
+template <typename T, typename Key>
+concept HashGenerator = requires(T t, size_t n, Key k) {
+  typename T::hash_type;
+  T{n};
+  { t.hashes_per_key() } -> std::convertible_to<std::size_t>;
+  { t.hashes(k) } -> std::ranges::range;
+  requires std::same_as<std::ranges::range_value_t<decltype(t.hashes(k))>,
+                        typename T::hash_type>;
 } && std::unsigned_integral<typename T::hash_type>;
 
-template<typename T, typename Key>
-concept HashFunction = requires (T t) {
-    typename T::hash_type;
-    typename T::seed_type;
-    std::unsigned_integral<typename T::hash_type>;
-    requires requires (Key& k, T::seed_type s) {{t.operator()(k,s)} -> std::same_as<typename T::hash_type>;};
+template <typename T, typename Key>
+concept HashFunction = requires(T t) {
+  typename T::hash_type;
+  typename T::seed_type;
+  std::unsigned_integral<typename T::hash_type>;
+  requires requires(Key &k, T::seed_type s) {
+    { t.operator()(k, s) } -> std::same_as<typename T::hash_type>;
+  };
 };
 
-
-template <typename Key, HashFunction<Key> Hash>
-class simple_hash_generator {
+template <typename Key, HashFunction<Key> Hash> class simple_hash_generator {
 public:
-    using seed_type = typename Hash::seed_type;
-    using hash_type = typename Hash::hash_type;
-    simple_hash_generator(size_t hashes_per_key) : _hashes_per_key(hashes_per_key)
-    {}
-    auto hashes(const Key& key) const {
-        return std::views::iota(0u, _hashes_per_key) | std::views::transform([&key](seed_type seed){
-            return Hash{}(key,seed);
-        });
-    }
-    
-    size_t hashes_per_key() const {
-        return _hashes_per_key;
-    }
-    
+  using seed_type = typename Hash::seed_type;
+  using hash_type = typename Hash::hash_type;
+  simple_hash_generator(size_t hashes_per_key)
+      : _hashes_per_key(hashes_per_key) {}
+  auto hashes(const Key &key) const {
+    return std::views::iota(0u, _hashes_per_key) |
+           std::views::transform(
+               [&key](seed_type seed) { return Hash{}(key, seed); });
+  }
+
+  size_t hashes_per_key() const { return _hashes_per_key; }
+
 private:
-    size_t _hashes_per_key;
+  size_t _hashes_per_key;
 };
 
-template <typename Key, HashFunction<Key> Hash, typename Allocator = std::allocator<typename Hash::seed_type>>
+template <typename Key, HashFunction<Key> Hash,
+          typename Allocator = std::allocator<typename Hash::seed_type>>
 class seeded_hash_generator {
 public:
-    using seed_type = typename Hash::seed_type;
-    using hash_type = typename Hash::hash_type;
-    seeded_hash_generator(size_t num_hashes, unsigned int rng_seed = std::random_device{}(), const Allocator& alloc = Allocator()) : _seeds(num_hashes, alloc) {
-        auto engine =std::mt19937_64{rng_seed};
-        auto dist = std::uniform_int_distribution<seed_type>{std::numeric_limits<seed_type>::min(), std::numeric_limits<seed_type>::max()};
-        std::unordered_set<seed_type,std::hash<Key>,std::equal_to<Key>,Allocator> previous_seeds(alloc);
-        for (auto& seed:  _seeds) {
-            seed = dist(engine);
-            while (previous_seeds.contains(seed)) seed = dist(engine);
-            previous_seeds.insert(seed);
-        }
+  using seed_type = typename Hash::seed_type;
+  using hash_type = typename Hash::hash_type;
+  seeded_hash_generator(size_t num_hashes,
+                        unsigned int rng_seed = std::random_device{}(),
+                        const Allocator &alloc = Allocator())
+      : _seeds(num_hashes, alloc) {
+    auto engine = std::mt19937_64{rng_seed};
+    auto dist = std::uniform_int_distribution<seed_type>{
+        std::numeric_limits<seed_type>::min(),
+        std::numeric_limits<seed_type>::max()};
+    std::unordered_set<seed_type, std::hash<Key>, std::equal_to<Key>, Allocator>
+        previous_seeds(alloc);
+    for (auto &seed : _seeds) {
+      seed = dist(engine);
+      while (previous_seeds.contains(seed))
+        seed = dist(engine);
+      previous_seeds.insert(seed);
     }
-    seeded_hash_generator(std::initializer_list<seed_type> seeds, const Allocator& alloc = Allocator()) : _seeds(seeds, alloc) {}
-    auto hashes(const Key& key) const {
-        return _seeds | std::views::transform([&](seed_type seed){
-            return Hash{}(key,seed);
-        });
-    }
-    
-    std::vector<seed_type, Allocator> get_seeds() const {
-        return _seeds;
-    }
-    size_t hashes_per_key() const {
-        return _seeds.size();
-    }
-    
+  }
+  seeded_hash_generator(std::initializer_list<seed_type> seeds,
+                        const Allocator &alloc = Allocator())
+      : _seeds(seeds, alloc) {}
+  auto hashes(const Key &key) const {
+    return _seeds | std::views::transform(
+                        [&](seed_type seed) { return Hash{}(key, seed); });
+  }
+
+  std::vector<seed_type, Allocator> get_seeds() const { return _seeds; }
+  size_t hashes_per_key() const { return _seeds.size(); }
+
 private:
-    std::vector<seed_type, Allocator> _seeds;
-}; 
-
-template <typename Key>
-struct murmer3_x64_128 {
-    using seed_type = uint32_t;
-    using hash_type = uint64_t;
-    hash_type operator()(const Key& key, seed_type seed) {
-        uint64_t hash[2];
-        MurmurHash3_x64_128(&key, sizeof(Key), seed, hash);
-        return hash[0];
-    }
+  std::vector<seed_type, Allocator> _seeds;
 };
 
-template <typename Key>
-struct murmer3_x86_32 {
-    using seed_type = uint32_t;
-    using hash_type = uint32_t;
-    hash_type operator()(const Key& key, seed_type seed) {
-        uint32_t hash;
-        MurmurHash3_x86_32(&key, sizeof(Key), seed, &hash);
-        return hash;
-    }
+template <typename Key> struct murmer3_x64_128 {
+  using seed_type = uint32_t;
+  using hash_type = uint64_t;
+  hash_type operator()(const Key &key, seed_type seed) {
+    uint64_t hash[2];
+    MurmurHash3_x64_128(&key, sizeof(Key), seed, hash);
+    return hash[0];
+  }
 };
-template<typename Key>
-using default_hash = murmer3_x86_32<Key>;
 
-template<typename Key>
-using default_hash_generator = simple_hash_generator<Key,default_hash<Key>>;
-}
-}
+template <typename Key> struct murmer3_x86_32 {
+  using seed_type = uint32_t;
+  using hash_type = uint32_t;
+  hash_type operator()(const Key &key, seed_type seed) {
+    uint32_t hash;
+    MurmurHash3_x86_32(&key, sizeof(Key), seed, &hash);
+    return hash;
+  }
+};
+template <typename Key> using default_hash = murmer3_x86_32<Key>;
+
+template <typename Key>
+using default_hash_generator = simple_hash_generator<Key, default_hash<Key>>;
+} // namespace hash
+} // namespace pds
 
 #endif
